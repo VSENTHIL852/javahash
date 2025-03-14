f09906212204ef23de0e09efd8868823465dc64d
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-03-14 10:55:15 +0530

    Create Hash.java

---
+++ Hash.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        HashMap<String, Double> map=new HashMap<>();
+        map.put("apple",0.10);
+        map.put("grapes",0.50);
+        map.put("orange",100.0);
+        map.put("wine",10000.0);
+        System.out.println(map);
+        map.remove("apple");
+        System.out.println(map);
+        System.out.println(map.get("orange"));
+        if(map.containsKey("wine")){
+            System.out.print("value of wine is:");
+            System.out.println(map.get("wine"));
+        }
+        else{
+            System.out.println(map.get("value not found"));
+        }
+        System.out.println("ovrall size of the map is:" +map.size()); 
+        
+    }
+}
